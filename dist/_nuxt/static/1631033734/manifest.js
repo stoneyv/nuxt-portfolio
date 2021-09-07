f1ef8d8c08ff4281c0aafa36127d7c82300c27b5
Stoney Vintson <stoneyv@gmail.com>
2021-09-07 12:27:43 -0500

    remove dist build dir from gitignore

---
+++ dist/_nuxt/static/1631033734/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fside-about","\u002Fpost","\u002Fpost\u002Fcargo_ocr","\u002Fpost\u002Fcbsa_plot","\u002Fpost\u002Ffars","\u002Fpost\u002Fpower_plant","\u002Fpost\u002Fcdc_diabetes","\u002Fpost\u002Ftopics","\u002Fpost\u002Ftutorial"]})
